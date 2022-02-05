64868a80c73f8b8aa3fcb1e19ec9feb2098cc29a
asinev <alexander.sinev@solbeg.com>
2022-02-05 20:48:46 +0300

    add init_data.sql (fixed)

--- task/src/main/resources/db/changelog/init_data.sql
+++ task/src/main/resources/db/changelog/init_data.sql
@@ -1,18 +1,3 @@
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (1, 'Иванов Иван Иваныч', 435 , 'assistant');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (2, 'Олегов Олег Олегович', 215 , 'associate professor');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (3, 'Карпин Валейри Яковлевич', 195 , 'professor');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (4, 'Петров Петр Петрович', 564 , 'professor');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (5, 'Сидоров Василий Иванович', 789 , 'professor');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (6, 'Андреев Андрей Андреевич', 564 , 'associate professor');
-INSERT INTO public.lector (id, name, salary,degree_id)
-VALUES (7, 'Колин Николай Николаевич', 235 , 'assistant');
-
 INSERT INTO public.degree (id, title)
 VALUES (1, 'ASSISTANT');
 INSERT INTO public.degree (id, title)
@@ -20,12 +5,31 @@ VALUES (2, 'ASSOCIATE_PROFESSOR');
 INSERT INTO public.degree (id, title)
 VALUES (3, 'PROFESSOR');
 
-
 INSERT INTO public.department (id, name)
 VALUES (1, 'TO');
 INSERT INTO public.department (id, name)
-VALUES (1, 'MO');
+VALUES (2, 'MO');
 INSERT INTO public.department (id, name)
-VALUES (1, 'EKO');
+VALUES (3, 'EKO');
 INSERT INTO public.department (id, name)
-VALUES (1, 'MFU');
+VALUES (4, 'MFU');
+
+
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (1, 'Иванов Иван Иваныч', 435 , 1);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (2, 'Олегов Олег Олегович', 215 , 2);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (3, 'Карпин Валейри Яковлевич', 195 , 3);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (4, 'Петров Петр Петрович', 564 , 3);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (5, 'Сидоров Василий Иванович', 789 , 3);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (6, 'Андреев Андрей Андреевич', 564 , 2);
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (7, 'Колин Николай Николаевич', 235 , 1);
+
+
+
+
