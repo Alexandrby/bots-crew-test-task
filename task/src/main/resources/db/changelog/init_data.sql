13a61a05345c7aa02dc8ccda04195a7b365f8049
asinev <alexander.sinev@solbeg.com>
2022-02-05 20:40:35 +0300

    add init_data.sql

---
+++ task/src/main/resources/db/changelog/init_data.sql
@@ -0,0 +1,31 @@
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (1, 'Иванов Иван Иваныч', 435 , 'assistant');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (2, 'Олегов Олег Олегович', 215 , 'associate professor');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (3, 'Карпин Валейри Яковлевич', 195 , 'professor');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (4, 'Петров Петр Петрович', 564 , 'professor');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (5, 'Сидоров Василий Иванович', 789 , 'professor');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (6, 'Андреев Андрей Андреевич', 564 , 'associate professor');
+INSERT INTO public.lector (id, name, salary,degree_id)
+VALUES (7, 'Колин Николай Николаевич', 235 , 'assistant');
+
+INSERT INTO public.degree (id, title)
+VALUES (1, 'ASSISTANT');
+INSERT INTO public.degree (id, title)
+VALUES (2, 'ASSOCIATE_PROFESSOR');
+INSERT INTO public.degree (id, title)
+VALUES (3, 'PROFESSOR');
+
+
+INSERT INTO public.department (id, name)
+VALUES (1, 'TO');
+INSERT INTO public.department (id, name)
+VALUES (1, 'MO');
+INSERT INTO public.department (id, name)
+VALUES (1, 'EKO');
+INSERT INTO public.department (id, name)
+VALUES (1, 'MFU');
